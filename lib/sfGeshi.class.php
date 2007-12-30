6370cb859d11c658eb3bc523e13b238f44c0cf1c
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-30 05:10:27 -0600

    more sfGeshiPlugin -> dkGeshiPlugin refactoring
    git-svn-id: http://svn.symfony-project.com/plugins/dkGeshiPlugin/trunk@6828 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfGeshi.class.php
+++ lib/sfGeshi.class.php
@@ -2,91 +2,17 @@
 /**
  * sfGeshi
  *
- * Wrap the GeSHi library to use it transparently in symfony
+ * This class does not exists anymore, since plugin has been refactored to dkGeshiPlugin.
+ * Please use dkGeshi class instead
  *
+ * @see dkGeshi.class.php
  * @author Romain Dorgueil <hartym@dakrazy.net>
  * @link http://www.dakrazy.net/document/1-SfGeshiDocumentation.html
  */
-class sfGeshi extends GeSHi
+class sfGeshi
 {
-  protected static $plugin_path = null;
-
-  public function __construct($source, $language)
+  public function __call($m, $p)
   {
-    parent::__construct($source, $language);
-  }
-
-
-  public static function getPluginPath()
-  {
-    if (self::$plugin_path === null)
-    {
-      $_tmp = array_reverse(explode('/', realpath(dirname(__FILE__))));
-      self::$plugin_path = SF_ROOT_DIR . '/plugins/' . $_tmp[1];
-    }
-
-    return self::$plugin_path;
-  }
-
-  /**
-   * This functions hacks the GeSHi::set_language_path method to use symfony's path
-   *
-   * @param string $path
-   * @param boolean $override
-   */
-  function set_language_path ($path, $override=false)
-  {
-    static $_path = null;
-
-    if ($override)
-    {
-      parent::set_language_path($path);
-    }
-    else
-    {
-      if ($_path === null)
-      {
-        $_path = self::getPluginPath() . '/geshi/';
-      }
-
-      $this->language_path = $_path;
-      $this->set_language($this->language);
-    }
-  }
-
-  /**
-   * Returns an associative array of GeSHi language identifier => Human readable language name
-   *
-   */
-  public static function getLanguages()
-  {
-    static $result = null;
-
-    if ($result === null)
-    {
-      $cache_file = SF_ROOT_DIR . '/cache/sfGeshiLanguages.cache.php';
-
-      if (!file_exists($cache_file))
-      {
-        $files = sfFinder::type('file')->name('*.php')->in(self::getPluginPath().'/geshi/');
-        $result = array();
-
-        foreach ($files as $file)
-        {
-          require($file);
-
-          $result[basename($file, '.php')] = $language_data['LANG_NAME'];
-        }
-        asort($result);
-        
-        file_put_contents($cache_file, serialize($result));
-      }
-      else
-      {
-        $result = unserialize(file_get_contents($cache_file));
-      }
-    }
-
-    return $result;
+    throw new sfException('sfGeshi has been refactored to dkGeshi, please use this class instead.');
   }
 }
